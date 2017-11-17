bd631b24243698685337eee9b607046befc72f3f
Daniel-Hain-Trevino <33500989+Daniel-Hain-Trevino@users.noreply.github.com>
2017-11-16 19:09:15 -0600

    Added Game_over_window

--- Project/Window_manager.h
+++ Project/Window_manager.h
@@ -6,13 +6,15 @@
 #include "Help_window.h"
 #include "Initials_window.h"
 #include "Game_window.h"
+#include "Game_over_window.h"
 
 class Window_manager {
 	int init_main_window();
 	void init_help();
 	int init_difficulty();
 	void init_initials();
-	bool init_game(int difficulty);
+	void init_game(int difficulty);
+	bool init_game_over();
 	string user_initial;
 public:
 	Window_manager();
