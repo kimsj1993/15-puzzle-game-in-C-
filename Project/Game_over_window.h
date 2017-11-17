eff50712ebc676fb6d3db8eebbf282ecf58d44a1
Daniel-Hain-Trevino <33500989+Daniel-Hain-Trevino@users.noreply.github.com>
2017-11-16 19:13:29 -0600

    Create Game_over_window.h

---
+++ Project/Game_over_window.h
@@ -0,0 +1,28 @@
+#ifndef GAME_OVER_WINDOW
+#define GAME_OVER_WINDOW
+
+#include "Simple_window.h"
+
+class Game_over_window : Graph_lib::Window {
+        Rectangle background;
+        Text game_over_text;
+        Button play_button;
+        Button quit_button;
+        Rectangle play_button_cover;
+        Rectangle quit_button_cover;
+        Text play_button_text;
+        Text quit_button_text;
+
+        bool button_pushed;
+        int selection;
+        void stylize_objects();
+        void attach_objects();
+public:
+        Game_over_window(Point xy, int w, int h, const string& title);
+        void wait_for_button();
+        bool change_state(bool choice);
+        bool get_choice() { return selection; }
+        void quit();
+};
+//If user chooses play again just start the game again
+#endif
